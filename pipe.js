26d189d47454d388b92e4fe890a9f54ef4b4b3a0
wusfen <404315887@qq.com>
2019-05-09 19:06:37 +0800

    Create pipe.js

---
+++ pipe.js
@@ -0,0 +1,17 @@
+function pipe(){
+  var args = [].splice.call(arguments, 0)
+  var fn = function(callback){
+    args = args.concat([].splice.call(arguments, 1))
+    var value = callback.apply(fn.value, args)
+    fn.value = value
+    fn.args = args
+    fn.valueOf = fn.toString = function(){
+      return value
+    }
+    args = [value]
+    return fn
+  }
+  fn.value = args[0]
+  fn.args = args
+  return fn
+}
